7a7d5510f8470dbd2ba38128feaf7d4da4c23013
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-08 19:13:34 +0200

    revamped js set-up for weather

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       ready: true,
       coordinates: response.data.coordinates,
       temperature: response.data.temperature,
-      humidity: response.data.main.humidity,
+      humidity: response.data.temperature.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.condition.description,
       icon: response.data.condition.icon,
@@ -63,41 +63,14 @@ export default function Weather(props) {
                 />
               </div>
             </form>
-          </div>
-          <div className="col-2">
-            <span className="date" id="date">
+            </div>
+            </div>
             <WeatherInfo data={weatherData} />
-            </span>
-          </div>
-        </div>
-
-<div className="row myRow">
-    <div className="col-5 todayDetails">
-        <p className="cityName" id="city">{props.defaultCity}</p>
-        <div className="temperature-icon">
-            <span className="tempElement" id="temperature">{weatherData.temperature}&deg;C</span>
-            <span className="temperatureUnits">C</span>
-        </div>
-        <small>
-            Humidity: <span className="hum" id="hum">80</span>%
-            <br />
-            Wind: <span className="wind" id="wind">7 </span>
-            <span className="windUnit">km/h</span>
-        </small>
-    </div>
-    <div className="col-6">
-        <h5 className="popup" id="description">Today's weather is warm
-        </h5>
-    </div>
-</div>
-<div className="row g-0 col-12 weather-forecast" id="forecast">
-<WeatherForecast coordinates={weatherData.coordinates} />
-</div>
-                </div>
-                </div>
-               
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+      </div>
 );
 } else {
-return "Loading..."
-}
-}
+    search();
+return "Loading...";
+} }
