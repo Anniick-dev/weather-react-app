cd7ed530e2a7dcb50bc423be5aabe39b4275a20e
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-08 18:58:11 +0200

    Added defaultcity

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState(props.defaultCity);
+
+  useEffect(() => {
+    search();
+  }, []);
 
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      city: response.data.city,
     });
   }
 
@@ -32,151 +36,92 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
+    let apiURL=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
   }
 
-  if (weatherData.ready) {
-    return (
-        <div className="container">
-            <div className="card">
-            <div className="row">
-                <div className="col-2">
-                    <label className="switch">
-                        <input type="checkbox" className="switch-control-input"/>
-                            <span className="slider round"></span>
-                        </label>
-                    <div className="switch-text">Celsius --- Fahrenheit</div>
-                </div>
-                <div className="col-8 m-0">
-                    <form className="search-button" onSubmit={handleSubmit}>
-                        <div className="input-group">
-                            <input
-                                type="text"
-                                placeholder="Search for your city"
-                                className="city-input"
-                                id="city-input"
-                                onChange={handleCityChange}
-                                autocomplete="on"
-                                autofocus="on"/>
-                            </div>
-                    </form>
-                
-                <div className="col-2">
-                    <span className="date" id="date"> october 8 </span>
-                </div>
-
-                <WeatherInfo data={weatherData} />
-                <WeatherForecast coordinates={weatherData.coordinates} />
-
-            </div>
-
-        </div>
-
-        <div className="row myRow">
-            <div className="col-5 todayDetails">
-                <p className="cityName" id="city">Amsterdam</p>
-                <div className="temperature-icon">
-                    <img src="" className="main-image" alt="Clear" id="icon" />
-                    <span className="tempElement" id="temperature">temperature</span>
-                    <span className="temperatureUnits">C</span>
-                </div>
-                <small>
-                    Humidity: <span className="hum" id="hum">80</span>%
-                    <br />
-                    Wind: <span className="wind" id="wind">7 </span>
-                    <span className="windUnit">km/h</span>
-                </small>
-            </div>
-            <div className="col-6">
-                <h5 className="popup" id="description">Today's weather is warm
-                </h5>
-            </div>
-        </div>
-        <div className="row g-0 col-12 weather-forecast" id="forecast">
-
-                       
-                        <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    
-    </div><div className="weather-forecast" id="forecast">
-        </div>
-        </div>
-        </div>
-    );
-} else {
-    search();
+  if (weatherData === null) {
     return "Loading...";
   }
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="row">
+          <div className="col-2">
+            <label className="switch">
+              <input type="checkbox" className="switch-control-input" />
+              <span className="slider round"></span>
+            </label>
+            <div className="switch-text">Celsius --- Fahrenheit</div>
+          </div>
+          <div className="col-8 m-0">
+            <form className="search-button" onSubmit={handleSubmit}>
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Search for your city"
+                  className="city-input"
+                  id="city-input"
+                  onChange={handleCityChange}
+                  autoComplete="on"
+                  autoFocus="on"
+                />
+              </div>
+            </form>
+          </div>
+          <div className="col-2">
+            <span className="date" id="date">
+              {weatherData.date}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {weatherData.ready && (
+        <>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+        </>
+      )}
+
+<div className="row myRow">
+    <div className="col-5 todayDetails">
+        <p className="cityName" id="city">{props.defaultCity}</p>
+        <div className="temperature-icon">
+            <span className="tempElement" id="temperature">{weatherData.temperature}&deg;C</span>
+            <span className="temperatureUnits">C</span>
+        </div>
+        <small>
+            Humidity: <span className="hum" id="hum">80</span>%
+            <br />
+            Wind: <span className="wind" id="wind">7 </span>
+            <span className="windUnit">km/h</span>
+        </small>
+    </div>
+    <div className="col-6">
+        <h5 className="popup" id="description">Today's weather is warm
+        </h5>
+    </div>
+</div>
+<div className="row g-0 col-12 weather-forecast" id="forecast">
+
+               
+                <div className="col d-flex justify-content-center">
+                <button className="bubbles">
+                <span className="emoji">☀️</span>
+
+                <span className="forecastTemperature">
+                    <span id="minTemp">8 | </span>
+                    <span id="maxTemp">18</span>
+                    </span>
+
+                </button>        
+                </div>
+                </div>
+                </div>
+               
+);
 }
+    
