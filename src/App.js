93f547bd59405be208adf53ca594604859ed4480
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-08 16:18:10 +0200

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
                         </label>
                     <div className="switch-text">Celsius --- Fahrenheit</div>
                 </div>
-                <div className="col-6 m-0">
-                    <div className="search-button">
+                <div className="col-8 m-0">
+                    <form className="search-button">
                         <div className="input-group">
                             <input
                                 type="text"
@@ -26,23 +26,10 @@ function App() {
                                 autocomplete="on"
                                 autofocus="on"/>
                             </div>
-                    </div>
-                <div className="col-1 m-0">
-                    <button
-                        type="submit"
-                        id="city-search"
-                        value="search"
-                        className="vergrootglas">
-                        <i className="fa fa-search"></i>🔍</button>
-                    <button
-                        type="button"
-                        id="current-position"
-                        value="search"
-                        className="pindrop">
-                        <i className="fa fa-search"></i>📍</button>
-                </div>
-                <div className="col-1">
-                    <span className="date" id="date">  </span>
+                    </form>
+                
+                <div className="col-2">
+                    <span className="date" id="date"> october 8 </span>
                 </div>
 
             </div>
@@ -51,27 +38,27 @@ function App() {
 
         <div className="row myRow">
             <div className="col-5 todayDetails">
-                <p className="cityName" id="city"></p>
+                <p className="cityName" id="city">Amsterdam</p>
                 <div className="temperature-icon">
                     <img src="" className="main-image" alt="Clear" id="icon" />
-                    <span className="tempElement" id="temperature"></span>
-                    <span className="temperatureUnits">&degC</span>
+                    <span className="tempElement" id="temperature">20</span>
+                    <span className="temperatureUnits">C</span>
                 </div>
                 <small>
-                    Humidity: <span className="hum" id="hum"></span>%
+                    Humidity: <span className="hum" id="hum">80</span>%
                     <br />
-                    Wind: <span className="wind" id="wind"></span>
+                    Wind: <span className="wind" id="wind">7 </span>
                     <span className="windUnit">km/h</span>
                 </small>
             </div>
             <div className="col-6">
-                <h5 className="popup" id="description">
+                <h5 className="popup" id="description">Today's weather is warm
                 </h5>
             </div>
         </div>
     </div><div className="weather-forecast" id="forecast">
         </div><footer className="github">
-            <a href="https://github.com/Anniick-dev/Weather-App-Plus"> Open-source code </a> by Annick
+            <a href="github.com/Anniick-dev/weather-react-app"> Open-source code </a> by Annick
         </footer>
         </div>
         </div>
