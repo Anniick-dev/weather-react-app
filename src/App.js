7d12414fe12bf01ab7c1ce49a524ec132c0540f8
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-08 17:35:58 +0200

    Placed core information inside Weather compontent

--- src/App.js
+++ src/App.js
@@ -1,63 +1,15 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
     
   return (
     <div className="App">
      <div className="container">
-        <div className="card">
-            <div className="row">
-                <div className="col-2">
-                    <label className="switch">
-                        <input type="checkbox" className="switch-control-input"/>
-                            <span className="slider round"></span>
-                        </label>
-                    <div className="switch-text">Celsius --- Fahrenheit</div>
-                </div>
-                <div className="col-8 m-0">
-                    <form className="search-button">
-                        <div className="input-group">
-                            <input
-                                type="text"
-                                placeholder="Search for your city"
-                                className="city-input"
-                                id="city-input"
-                                autocomplete="on"
-                                autofocus="on"/>
-                            </div>
-                    </form>
-                
-                <div className="col-2">
-                    <span className="date" id="date"> october 8 </span>
-                </div>
 
-            </div>
+        <Weather />
 
-        </div>
-
-        <div className="row myRow">
-            <div className="col-5 todayDetails">
-                <p className="cityName" id="city">Amsterdam</p>
-                <div className="temperature-icon">
-                    <img src="" className="main-image" alt="Clear" id="icon" />
-                    <span className="tempElement" id="temperature">20</span>
-                    <span className="temperatureUnits">C</span>
-                </div>
-                <small>
-                    Humidity: <span className="hum" id="hum">80</span>%
-                    <br />
-                    Wind: <span className="wind" id="wind">7 </span>
-                    <span className="windUnit">km/h</span>
-                </small>
-            </div>
-            <div className="col-6">
-                <h5 className="popup" id="description">Today's weather is warm
-                </h5>
-            </div>
-        </div>
-    </div><div className="weather-forecast" id="forecast">
-        </div>
         <footer className="github">
             <a href="github.com/Anniick-dev/weather-react-app" target="_blank"> Open-source code </a> by Annick
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,232 +1,62 @@
+
 import React from "react";
 import axios from "axios";
 
 export default function Weather () {
+    return (
+        <div className="container">
+<div className="card">
+            <div className="row">
+                <div className="col-2">
+                    <label className="switch">
+                        <input type="checkbox" className="switch-control-input"/>
+                            <span className="slider round"></span>
+                        </label>
+                    <div className="switch-text">Celsius --- Fahrenheit</div>
+                </div>
+                <div className="col-8 m-0">
+                    <form className="search-button">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                placeholder="Search for your city"
+                                className="city-input"
+                                id="city-input"
+                                autocomplete="on"
+                                autofocus="on"/>
+                            </div>
+                    </form>
+                
+                <div className="col-2">
+                    <span className="date" id="date"> october 8 </span>
+                </div>
 
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}&degC`);
-    let city = "amsterdam";
-        let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather/daily?q=${city}&appid=${apiKey}&units=metric`;
-    
-    axios.get(apiURL).then(handleResponse);
-    
-    return // Today's date on main screen//
+            </div>
 
-    function formatDate(date) {
-      let hours = date.getHours();
-      if (hours < 10) {
-        hours = `0${hours}`;
-      }
-      let minutes = date.getMinutes();
-      if (minutes < 10) {
-        minutes = `0${minutes}`;
-      }
-    
-      let dayIndex = date.getDay();
-      let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ];
-      let day = days[dayIndex];
-    
-      let dateNumber = date.getDate();
-    
-      let monthIndex = date.getMonth();
-      let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ]
-      let month = months[monthIndex];
-    
-      return `${day}, ${month} ${dateNumber} | ${hours}:${minutes}`;
-    }
-    
-    // function to search for specific day & date //
-    function getForecastDay(latitude, longitude) {
-      let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
-      let apiURL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&cnt=5`; // cnt=5 for 5-day forecast
-      axios.get(apiURL).then(displayForecast);
-    }
-    
-    function displayForecast(response) {
-      let forecastElement = document.querySelector("#forecast");
-    
-      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    
-      let forecastHTML = `<div class="row g-0 d-flex justify-content-center">`;
-    
-      response.data.daily.forEach(function (dayData) {
-        let timestamp = dayData.time * 1000;
-        let date = new Date(timestamp);
-        let dayName = days[date.getDay()];
-    
-     // Get month and day from the date //
-     let month = date.toLocaleString("default", { month: "short" });
-     let day = date.getDate();
-    
-        let minTemp = Math.round(dayData.temperature.minimum);
-        let maxTemp = Math.round(dayData.temperature.maximum);
-    
-        forecastHTML +=
-          `
-          <div class="col">
-            <button class="bubbles">
-              <img src="${dayData.condition.icon_url}" width=40px/>
-              <span class="forecastTemperature">
-                <span class="minTemp" id="minTemp">${minTemp}</span> |
-                <span class="maxTemp" id="maxTemp">${maxTemp}</span>
-              </span>
-            </button>
-            <span class="dayWeek">${dayName} | <span class="dateWeek"> ${month} ${day}</span></span>
-          </div>
-          `;
-      });
-    
-      forecastHTML += `</div>`;
-      forecastElement.innerHTML = forecastHTML;
-    }
-    
-    function getForecast(coordinates) {
-      console.log(coordinates);
-      let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
-      let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
-      axios.get(apiURL).then(displayForecast);
-    }
-    
-    // function to search for specific city //
-    function searchCity(city) { 
-      let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-      let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-      axios.get(apiURL).then(showWeather);  
-    
-      }
-    
-     // function to search for city //
-    function handleSubmit(event) {
-      event.preventDefault(); 
-      let city = document.querySelector("#city-input").value;
-      searchCity(city);
-    }
-    
-    function searchLocation(position) {
-      let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
-      let apiURL = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
-      
-      axios.get(apiURL).then(showWeather);  
-    }
-    
-      // function for current position //
-    function getCurrentLocation(event) {
-      event.preventDefault();
-      navigator.geolocation.getCurrentPosition(searchLocation);
-      }
-    
-      // showWeather //
-      function showWeather(response) {
-        document.querySelector("#city").innerHTML = response.data.city;
-        let celsiusTemperature = Math.round(response.data.temperature.current);
-        let feelTemp = Math.round(response.data.temperature.feels_like);
-        let descriptionText = response.data.condition.description;
-        document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
-        document.querySelector("#description").innerHTML = `It is a day filled with ${descriptionText} & feels like ${feelTemp}&degC. Enjoy!`;
-        document.querySelector("#hum").innerHTML = response.data.temperature.humidity;
-        document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-        
-        let iconElement = document.querySelector("#icon");
-        iconElement.setAttribute(
-            "src",
-            `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
-        iconElement.setAttribute(
-            "alt", response.data.condition.description);
-        
-        getForecast(response.data.coordinates);
-    }
-    
-    // calculate date //
-    let dateElement = document.querySelector("#date");
-    let currentTime = new Date();
-    dateElement.innerHTML = formatDate(currentTime);
-    
-    // push through search //
-    let searchForm = document.querySelector("form");
-    searchForm.addEventListener("submit", handleSubmit);
-      
-    // find current location //
-    let currentLocationButton = document.querySelector("#current-position");
-    currentLocationButton.addEventListener("click", getCurrentLocation);
-    
-    searchCity("New York");
-    
-    // Get slider to do something //
-    function updateTemperatureUnits() {
-      let temperatureElement = document.querySelector("#temperature");
-      let unitToggle = document.querySelector(".switch-control-input");
-      let temperatureUnitsElement = document.querySelector(".temperatureUnits");
-      let windElement = document.querySelector("#wind");
-      let windUnitElement = document.querySelector(".windUnit");
-    
-      let minTempElement = document.querySelector("#minTemp");
-      let maxTempElement = document.querySelector("#maxTemp");
-    
-      if (unitToggle.checked) {
-        // Celsius to Fahrenheit conversion for current temperature
-        let celsiusTemperature = parseFloat(temperatureElement.textContent);
-        let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-        temperatureElement.textContent = Math.round(fahrenheitTemperature);
-        temperatureUnitsElement.innerHTML = "&deg;F";
-    
-        // Celsius to Fahrenheit conversion for wind speed
-        let windCelsius = parseFloat(windElement.textContent);
-        let windFahrenheit = windCelsius * 0.621371;
-        windElement.textContent = Math.round(windFahrenheit);
-        windUnitElement.innerHTML = "m/h";
-    
-        // Celsius to Fahrenheit conversion for min and max temperatures
-        let minCelsiusTemperature = parseFloat(minTempElement.textContent);
-        let maxCelsiusTemperature = parseFloat(maxTempElement.textContent);
-        let minFahrenheitTemperature = (minCelsiusTemperature * 9) / 5 + 32;
-        let maxFahrenheitTemperature = (maxCelsiusTemperature * 9) / 5 + 32;
-        minTempElement.textContent = Math.round(minFahrenheitTemperature);
-        maxTempElement.textContent = Math.round(maxFahrenheitTemperature);
-      } else {
-        // Fahrenheit to Celsius conversion for current temperature
-        let fahrenheitTemperature = parseFloat(temperatureElement.textContent);
-        let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
-        temperatureElement.textContent = Math.round(celsiusTemperature);
-        temperatureUnitsElement.innerHTML = "&deg;C";
-    
-        // Fahrenheit to Celsius conversion for wind speed
-        let windFahrenheit = parseFloat(windElement.textContent);
-        let windCelsius = windFahrenheit / 0.621371;
-        windElement.textContent = Math.round(windCelsius);
-        windUnitElement.innerHTML = "km/h";
-    
-        // Fahrenheit to Celsius conversion for min and max temperatures
-        let minFahrenheitTemperature = parseFloat(minTempElement.textContent);
-        let maxFahrenheitTemperature = parseFloat(maxTempElement.textContent);
-        let minCelsiusTemperature = ((minFahrenheitTemperature - 32) * 5) / 9;
-        let maxCelsiusTemperature = ((maxFahrenheitTemperature - 32) * 5) / 9;
-        minTempElement.textContent = Math.round(minCelsiusTemperature);
-        maxTempElement.textContent = Math.round(maxCelsiusTemperature);
-      }
-    }
-    
-    var unitToggle = document.querySelector(".switch-control-input");
-    unitToggle.addEventListener("change", updateTemperatureUnits);
-    
- } 
+        </div>
+
+        <div className="row myRow">
+            <div className="col-5 todayDetails">
+                <p className="cityName" id="city">Amsterdam</p>
+                <div className="temperature-icon">
+                    <img src="" className="main-image" alt="Clear" id="icon" />
+                    <span className="tempElement" id="temperature">20</span>
+                    <span className="temperatureUnits">C</span>
+                </div>
+                <small>
+                    Humidity: <span className="hum" id="hum">80</span>%
+                    <br />
+                    Wind: <span className="wind" id="wind">7 </span>
+                    <span className="windUnit">km/h</span>
+                </small>
+            </div>
+            <div className="col-6">
+                <h5 className="popup" id="description">Today's weather is warm
+                </h5>
+            </div>
+        </div>
+    </div><div className="weather-forecast" id="forecast">
+        </div>
+        </div>
+    )
+}
