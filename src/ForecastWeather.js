d3d00d208e26415547382bd35628dad95d9158d3
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-09 17:48:20 +0200

    Prepped forecast layout

--- src/ForecastWeather.js
+++ src/ForecastWeather.js
@@ -1,70 +1,79 @@
 import React from "react";
+import './App.css';
 
 export default function ForecastWeather (){
     return (
     <div className="ForecastWeather">
     <div className="row g-0 col-12 m-0 weather-forecast" id="forecast">
 
-
 <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
+    <div className="row">
+        <button className="bubbles">
+        <span className="emoji">☀️</span>
+          <span className="forecastTemperature">
+            <span className="minTemp" id="minTemp">9 </span> |
+            <span className="maxTemp" id="maxTemp"> 20</span>
+          </span>
+        </button>
+        <span className="dayWeek">Monday | <span clclassNameass="dateWeek"> December 9</span></span>
+      </div>
+      </div>
 
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
+      <div className="col d-flex justify-content-center">
+    <div className="row">
+        <button className="bubbles">
+        <span className="emoji">☀️</span>
+          <span className="forecastTemperature">
+            <span className="minTemp" id="minTemp">9 </span> |
+            <span className="maxTemp" id="maxTemp"> 20</span>
+          </span>
+        </button>
+        <span className="dayWeek">Monday | <span clclassNameass="dateWeek"> December 9</span></span>
+      </div>
+      </div>
 
-                        </button>
+      <div className="col d-flex justify-content-center">
+    <div className="row">
+        <button className="bubbles">
+        <span className="emoji">☀️</span>
+          <span className="forecastTemperature">
+            <span className="minTemp" id="minTemp">9 </span> |
+            <span className="maxTemp" id="maxTemp"> 20</span>
+          </span>
+        </button>
+        <span className="dayWeek">Monday | <span clclassNameass="dateWeek"> December 9</span></span>
+      </div>
+      </div>
+
+      <div className="col d-flex justify-content-center">
+    <div className="row">
+        <button className="bubbles">
+        <span className="emoji">☀️</span>
+          <span className="forecastTemperature">
+            <span className="minTemp" id="minTemp">9 </span> |
+            <span className="maxTemp" id="maxTemp"> 20</span>
+          </span>
+        </button>
+        <span className="dayWeek">Monday | <span clclassNameass="dateWeek"> December 9</span></span>
+      </div>
+      </div>
+
+      <div className="col d-flex justify-content-center">
+    <div className="row">
+        <button className="bubbles">
+        <span className="emoji">☀️</span>
+          <span className="forecastTemperature">
+            <span className="minTemp" id="minTemp">9 </span> |
+            <span className="maxTemp" id="maxTemp"> 20</span>
+          </span>
+        </button>
+        <span className="dayWeek">Monday | <span className="dateWeek"> December 9</span></span>
+      </div>
+      </div>
+
+                
                     </div>
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
                     </div>
 
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="col d-flex justify-content-center">
-                        <button className="bubbles">
-                        <span className="emoji">☀️</span>
-
-                        <span className="forecastTemperature">
-                            <span id="minTemp">8 | </span>
-                            <span id="maxTemp">18</span>
-                            </span>
-
-                        </button>
-                    </div>
-                    </div>
-    </div>
     );
 }
